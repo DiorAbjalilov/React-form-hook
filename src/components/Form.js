1584dbcd1e25d63fa2715ee376b17b73d977c7f0
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-02-28 21:39:45 +0500

    fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,41 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+      const onSubmit = (data) => alert(JSON.stringify(data));
   return (
     <>
-        <div class="container">  
-            <form id="contact" action="" method="post">
+        <div className="container">  
+            <form onSubmit={handleSubmit(onSubmit)} id="contact" action="" method="post">
                 <h3>Contact Form</h3>
                 <h4>Contact us for custom quote</h4>
                 <fieldset>
-                <input placeholder="Your name" type="text" tabindex="1" required autofocus />
+                    <input placeholder="Your name" type="text" className={`${errors.firstName?'error':''}`} {...register('firstName', { required: true, minLength:6 })} tabIndex="1" autoFocus  />
+                {errors.firstName && <span className='errorMessage'>Last name is required and min length 6.</span>}
                 </fieldset>
                 <fieldset>
-                <input placeholder="Your Email Address" type="email" tabindex="2" required />
+                    <input placeholder="Your Email Address" type="text" className={`${errors.yourEmail?'error':''}`} {...register('yourEmail', { required: true, minLength:5, pattern: /^\S+@\S+$/i })} tabIndex="2" />
+                    {errors.yourEmail && <span className='errorMessage'>Email is required.</span>}
                 </fieldset>
                 <fieldset>
-                <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
+                    <input placeholder="Your Phone Number" type="tel" className={`${errors.yourEmail?'error':''}`} tabIndex="3" />
+                    {errors.yourEmail && <span className='errorMessage'>Phone number is required.</span>}
                 </fieldset>
                 <fieldset>
-                <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required />
+                    <input placeholder="Your Web Site (optional)" type="text" {...register('yourWebSite')} tabIndex="4"  />
                 </fieldset>
                 <fieldset>
-                <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+                    <textarea placeholder="Type your message here...." {...register('messageText', { required: true })} tabIndex="5"></textarea>
                 </fieldset>
                 <fieldset>
-                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
                 </fieldset>
-                <p class="copyright">Designed by <a href="https://github.com/DiorAbjalilov" target="_blank" title="Colorlib">Colorlib</a></p>
+                    <p className="copyright">Designed by <a href="https://github.com/DiorAbjalilov" target="_blank" title="Colorlib">Programmer</a></p>
             </form>
         </div>
     </>
