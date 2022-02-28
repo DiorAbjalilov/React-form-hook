58ef688dbf340155cbe82923778fac3d516cb9d9
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-02-28 21:56:13 +0500

    finsh

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,27 +15,58 @@ const Form = () => {
                 <h3>Contact Form</h3>
                 <h4>Contact us for custom quote</h4>
                 <fieldset>
-                    <input placeholder="Your name" type="text" className={`${errors.firstName?'error':''}`} {...register('firstName', { required: true, minLength:6 })} tabIndex="1" autoFocus  />
+                    <input 
+                    placeholder="Your name" 
+                    type="text" className={`${errors.firstName?'error':''}`} 
+                    {...register('firstName', { required: true, minLength:6 })} 
+                    tabIndex="1" 
+                    autoFocus  
+                    />
                 {errors.firstName && <span className='errorMessage'>Last name is required and min length 6.</span>}
                 </fieldset>
                 <fieldset>
-                    <input placeholder="Your Email Address" type="text" className={`${errors.yourEmail?'error':''}`} {...register('yourEmail', { required: true, minLength:5, pattern: /^\S+@\S+$/i })} tabIndex="2" />
+                    <input 
+                        placeholder="Your Email Address" 
+                        type="text" className={`${errors.yourEmail?'error':''}`} 
+                        {...register('yourEmail', { required: true, minLength:5, pattern: /^\S+@\S+$/i })} 
+                        tabIndex="2" 
+                    />
                     {errors.yourEmail && <span className='errorMessage'>Email is required.</span>}
                 </fieldset>
                 <fieldset>
-                    <input placeholder="Your Phone Number" type="tel" className={`${errors.yourEmail?'error':''}`} tabIndex="3" />
-                    {errors.yourEmail && <span className='errorMessage'>Phone number is required.</span>}
+                    <input 
+                        placeholder="Your Phone Number" 
+                        type="tel" className={`${errors.phoneNumber?'error':''}`} 
+                        {...register('phoneNumber', {required:true, minLength:5, maxLength:15} )} 
+                        tabIndex="3" 
+                    />
+                    {errors.phoneNumber && <span className='errorMessage'>Phone number is required.</span>}
                 </fieldset>
                 <fieldset>
-                    <input placeholder="Your Web Site (optional)" type="text" {...register('yourWebSite')} tabIndex="4"  />
+                    <input 
+                        placeholder="Password" 
+                        type="password" 
+                        className={`${errors.password?'error':''}`} 
+                        {...register('password', {required:true, minLength:6, pattern:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/})} 
+                        tabIndex="4"
+                    />
+                    {errors.password && <span className='errorMessage'>Please check the Password</span>}
                 </fieldset>
                 <fieldset>
-                    <textarea placeholder="Type your message here...." {...register('messageText', { required: true })} tabIndex="5"></textarea>
+                    <textarea 
+                    placeholder="Type your message here...." 
+                    {...register('messageText', { required: true })} 
+                    tabIndex="5"
+                    ></textarea>
                 </fieldset>
                 <fieldset>
-                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                    <button 
+                    name="submit" 
+                    type="submit" 
+                    id="contact-submit" 
+                    data-submit="...Sending">Submit</button>
                 </fieldset>
-                    <p className="copyright">Designed by <a href="https://github.com/DiorAbjalilov" target="_blank" title="Colorlib">Programmer</a></p>
+                    <p className="copyright">Designed by <a href="https://github.com/DiorAbjalilov" target="_blank" title="Abjalilov Diyor">Programmer</a></p>
             </form>
         </div>
     </>
